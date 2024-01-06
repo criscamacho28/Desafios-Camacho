d34f3cf85efefa08225e6058b5008d907be8d553
Cristian Camacho <cristian10camacho@gmail.com>
2024-01-05 22:01:20 -0300

    correcciones

--- ProductManager.js
+++ ProductManager.js
@@ -1,19 +1,57 @@
-class ProductManager {
+class ProductManager {	//Este ProductManager gestiona un conjunto de productos
 	constructor() {
-		this.products = [];
+		this.products = []; //Los metodos creados trabajaran sobre este array.
 	}
 
-	addProduct(title, description, price, thumbnail, code, stock) {
-		// --- Escribe aqui tu codigo ---
+	static id = 0; //Esto sirve para crear un id automatico
+
+	addProduct(title, description, price, thumbnail, code, stock) { //Este es un metodo que recibe y agrega los productos
+		//Recibe lo que esta en los parentesis
+
+		for(let i = 0; i < this.products.length; i += 1){ //Esto es para cuando el code existe
+			if(this.products[i].code === code){
+				console.log(`El codigo ${code} esta repetido`);
+				break;
+			}
+		}
+
+		const newProduct = {
+			title,
+    		description,
+    		price,
+    		thumbnail,
+    		code,
+    		stock,
+		}
+		if(!Object.values(newProduct).includes(undefined)){
+			ProductManager.id++; //Esto le suma 1 al id
+	
+			this.products.push({
+				...newProduct,
+				id: ProductManager.id
+			}); //Recibe todo estas caracteristicas, las transforma
+			//en un obj y por medio de .push lo sube a products
+		}else{
+			console.log("Todos los campos son requeridos");
+		}
+
 	}
 
-	getProducts() {
-		// --- Escribe aqui tu codigo ---
+	existe(id){
+		return this.products.find((manager) => manager.id === id);
 	}
 
-	getProductById(id) {
-		// --- Escribe aqui tu codigo ---
+	getProducts() { //Este metodo devuelve el arreglo con los productos creados
+		//No debe recibir nada como parametro
+		return this.products;
+	}
+
+	getProductById(id) { //Este metodo recibe un id y en caso que no este el id se mostrara error.
+		
+		//Si existe el id ...
+		!this.existe(id) ?  console.log("Not Found") : console.log("Existe el id y es el siguiente: "), console.log(this.existe(id));
 	}
 }
+//const manager = new ProductManager();
 
-const manager = new ProductManager();
+module.exports = ProductManager; //Esto es para exportar este archivo
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     Desafio Entregable 1
     Cristian Camacho
 */
-let product1 = {
+/* let product1 = {
     title: "Producto 1",
     description: "Este es un producto prueba 1",
     price: 100,
@@ -19,30 +19,65 @@ let product2 = {
     stock: 50,
 };
 let product3 = {
-    title: "Producto 1",
+    title: "Producto 3",
     description: "Este es un producto prueba 3",
     price: 300,
     thumbnail: "Sin imagen",
     code: "abc333",
     stock: 50,
 }
-const ProductManager = require("./ProductManager.js"); //importo otro archivo que tiene una clase
-const manager = new ProductManager();
+let product4 = {
+    title: "Producto 4",
+    description: "Este es un producto prueba 4",
+    price: 300,
+    thumbnail: "Sin imagen",
+    code: "abc333",
+    stock: 50,
+} */
+const ProductManager = require("./ProductManager"); //importo otro archivo que tiene una clase
+const manager = new ProductManager(); 
 
-console.log("------------------------");
+console.log("---------------------------------------------------------------------------------------------");
 console.log("Añadimos los productos");
-manager.addProduct(product1);
-manager.addProduct(product2);
-manager.addProduct(product3);
+manager.addProduct(
+    "Producto 1",
+    "Este es un producto prueba 1",
+    100,
+    "Sin imagen",
+    "abc111",
+    50);
+manager.addProduct(
+    "Producto 2",
+    "Este es un producto prueba 2",
+    200,
+    "Sin imagen",
+    "abc222",
+    50
+);
+manager.addProduct(
+    "Producto 3",
+    "Este es un producto prueba 3",
+    300,
+    "Sin imagen",
+    "abc333",
+    50
+);
 console.log("------------------------");
 console.log("Todos los productos que tengo: ");
 console.log(manager.getProducts());
 console.log("------------------------");
 console.log("Buscar producto por ID 2:");
-console.log(manager.getProductsById(2));
+console.log(manager.getProductById(2));
 console.log("------------------------");
 console.log("Buscar producto por ID inexistente:");
-console.log(manager.getProductsById(99));
+console.log(manager.getProductById(99));
 console.log("------------------------");
 console.log("Intento agregar un producto con codigo existente: ");
-manager.addProduct(product3);
+manager.addProduct(
+    "Producto 4",
+    "Este es un producto prueba 4",
+    100,
+    "Sin imagen",
+    "abc333",
+    50
+);
